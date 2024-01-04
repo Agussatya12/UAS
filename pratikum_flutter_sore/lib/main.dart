023b1477d0392bf31c4f48a253867f5a1c72bb08
dwiisinta <sintamahayani61@gmail.com>
2024-01-04 22:23:49 +0800

    test

---
+++ pratikum_flutter_sore/lib/main.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50))),
+          toolbarHeight: 230, // default is 56
+          toolbarOpacity: 0.9,
+          title: Column(
+            children: [
+              Center(
+                child: Container(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('lib/assets/foto.jpg'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Text(
+                  "Agus Satya",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text("Student", style: TextStyle(height: 1, fontSize: 14))
+            ],
+          ),
+        ),
+        body: Center(
+          child: Container(
+            width: 400,
+            margin: EdgeInsets.only(top: 35),
+            child: Column(
+              children: [
+                Text("Tentang Saya", style: TextStyle(fontWeight: FontWeight.bold)),
+                Container(
+                    margin: EdgeInsets.all(15),
+                    child: Text(
+                        "Hai, saat ini saya kuliah di undiknas dan skill saya membuat server PPOE Dari Mikrotik dan saya suka bermain game simulator ",
+                        maxLines: 4,
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(color: Colors.black.withOpacity(0.5)))),
+                Container(
+                  width: 155,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.1))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Kemampuan",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ),
+                Container(
+                    child: SizedBox(
+                  width: 300,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "5",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Gaming",
+                                    style: TextStyle(
+                                        color: Colors.black.withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "4",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Playstation",
+                                    style: TextStyle(
+                                        color: Colors.black.withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "3",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Nintendo Switch",
+                                    style: TextStyle(
+                                        color: Colors.black.withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "3",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Chill",
+                                    style: TextStyle(
+                                        color: Colors.black.withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  width: 155,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.1))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Temukan Saya Di",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Icon(
+                                Icons.facebook,
+                                color: Colors.blueAccent,
+                                size: 20.0,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Icon(
+                                Icons.tiktok,
+                                color: Colors.blueAccent,
+                                size: 20.0,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Icon(
+                                Icons.telegram,
+                                color: Colors.blueAccent,
+                                size: 20.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ]),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
